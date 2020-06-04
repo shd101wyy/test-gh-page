c4d885970c92b0a333f6e3b5feeb9845df4807b0
Yiyi Wang <shd101wyy@gmail.com>
2020-06-04 16:40:24 +0800

    Updates

---
+++ precache-manifest.fd6391ca551faef44ebb343444525821.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c2431d207d9fb721035edcc29f84e4",
+    "url": "/test-gh-page/index.html"
+  },
+  {
+    "revision": "dc95de61c999e9112fc8",
+    "url": "/test-gh-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d467fb69d3c980a5378",
+    "url": "/test-gh-page/static/js/2.adfd6169.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-gh-page/static/js/2.adfd6169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc95de61c999e9112fc8",
+    "url": "/test-gh-page/static/js/main.3a733443.chunk.js"
+  },
+  {
+    "revision": "75943708aeeeade33a7c",
+    "url": "/test-gh-page/static/js/runtime-main.24783f65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-gh-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
